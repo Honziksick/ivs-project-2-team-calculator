c6511c6cc4dded64f414e561361e624cdb6f77ab
David Krejčí <xkrejcd00@stud.fit.vutbr.cz>
2024-03-20 16:07:32 +0100

    Přídány testy a návrhy hlavních funkcí
    Přídány testy pro obecné i hraniční případy.
    Přidány následující funkce, podrobnější popis v cat_calc_core.h:
    calculate - Vypočítá zadaný výraz.
                Hlavní funkce kalkulačky, ostatní funkce se volají z ní.
    formatCheck - Kontroluje syntax zadaného výrazu.
    parse - Převede daný výraz na tokeny.
    postfix - Uspořádá tokeny do postfixového tvaru.
    evaluate - Vypočítá výsledek z postfixového tvaru.
    absVal - Absolutní hodnota.
    root - Odmocnina
    power - Mocnina
    csin - Vlastní sinus
    ccos - Vlastní cosinus
    ctan - Vlastní tangens

--- src/CalmCatCalc/core/cat_calc_core.h
+++ src/CalmCatCalc/core/cat_calc_core.h
@@ -10,9 +10,157 @@
 #include <stdexcept>
 #include <iostream>
 #include <algorithm>
+#include <string>
+
+using namespace std;
+
+/**
+ * @brief Výčet možných výjimek
+*/
+enum exceptionCodes{
+    // TODO Přidat detailní popis případů vyjímek
+    /**
+     * @brief Pro špatný zápis výrazu.
+     * 
+     * @example 64/*8
+    */
+    SyntaxError = 1,
+    /**
+     * @brief Pro nesedící páry závorek.
+     * 
+     * @example 6+8)*2
+    */
+    ParenthesisMismatch = 2,
+    /**
+     * @brief Pro neplatné matematické operace.
+     * 
+     * @example 5/0
+     * @example 4.2!
+     * @example 10^-2
+    */
+    MathError = 3,
+    /**
+     * @brief Pro přetečení maximální hodnoty double.
+    */
+    Overflow = 4
+};
 
 
-const char* calculate(const char* expression);
+/******************************************************************************
+ * Funkce pro práci s výrazy
+ * 
+******************************************************************************/
+
+/**
+ * @brief Výpočet hodnoty zadaného výrazu.
+ * 
+ * @param[in] expression Výraz, který má být vyhodnocen.
+ * @return Výsledek zadaného výrazu v podobě řetězce.
+ * @warning Nekontroluje neplatné znaky.
+ *          Platné znaky jsou: bílé znaky, čísla, operandy + - * / ^ # s c t
+ *          # značí odmocninu, kde 3#27 je třetí odmocnina z 27
+ *          s, c, t značí sinus, cosinus a tangens 
+ * @exception SyntaxError
+ * @exception ParenthesisMismatch
+ * @exception MathError
+ * @exception Overflow
+*/
+string calculate(string expression);
+
+/**
+ * @brief Kontrola zadaného výrazu.
+ * 
+ * @param[in] expression Vstupní výraz, který má být zkontrolován.
+ * @exception SyntaxError Pro špatný zápis výrazu.
+*/
+void formatCheck(string expression);
+
+/**
+ * @brief Rozděluje vstupní výraz na jednotlivé tokeny.
+ * 
+ * @param[in] expression Vstupní výraz, který má být rozdělen.
+ * @return Vektor s tokeny výrazu.
+*/
+vector<string> parse(string expression);
+
+/**
+ * @brief Převádí vektor tokenů z infixového tvaru na postfixový.
+ * 
+ * @param[in] parsedExpression Vektor s tokeny výrazu v infixovém tvaru.
+ * @return Vektor s tokeny výrazu v postfixovém tvaru.
+ * @exception ParenthesesMismatch Pro nesedící páry závorek.
+*/
+vector<string> postfix(vector<string> parsedExpression);
+
+/**
+ * @brief Vyhodnocuje výraz postfixového tvaru.
+ * 
+ * @param[in] postfixExpression Vektor s tokeny výrazu v postfixovém tvaru.
+ * @return Výsledek výrazu v podobě řetězce.
+ * @exception MathError Pro neplatné matematické operace jako dělení 0.
+*/
+string evaluate(vector<string> postfixExpression);
+
+
+
+/******************************************************************************
+ * Funkce matematických operací
+ * 
+******************************************************************************/
+
+
+/**
+ * @brief Výpočet absolutní hodnoty zadaného čísla.
+ * 
+ * @param[in] num Číslo, které má být převedeno na absolutní hodnotu.
+ * @return Absolutní hodnota zadaného čísla.
+*/
+double absVal(double num);
+
+/**
+ * @brief Výpočet odmocniny zadaného čísla na zadaný základ.
+ * 
+ * @param[in] root Základ odmocniny.
+ * @param[in] num Číslo, které má být odmocněno.
+ * @return Odmocnina zadaného čísla.
+ * @exception MathError Pro odmocninu ze záporného čísla se sudým základem
+ * @exception MathError Pro odmocninu se záporným základem.
+*/
+double root(int root, double num);
+
+/**
+ * @brief Výpočet mocniny zadaného čísla o zadaném exponentu.
+ * 
+ * @param[in] exp Exponent mocniny.
+ * @param[in] num Základ mocniny.
+ * @return Mocnina zadaného čísla.
+ * @exception MathError Pro umocňování záporným exponentem.
+*/
+double power(int exp, double num);
+
+/**
+ * @brief Výpočet sinu zadaného čísla.
+ * 
+ * @param[in] ang Úhel ve stupních.
+ * @return Sinus zadaného úhlu.
+*/
+double csin(double ang);
+
+/**
+ * @brief Výpočet cosinu zadaného čísla.
+ * 
+ * @param[in] ang Úhel ve stupních.
+ * @return Cosinus zadaného úhlu.
+*/
+double ccos(double ang);
+
+/**
+ * @brief Výpočet tangens zadaného čísla.
+ * 
+ * @param[in] ang Úhel ve stupních.
+ * @return Tangens zadaného úhlu.
+*/
+double ctan(double ang);
 
 
 #endif // CAT_CALC_CORE_H_
--- src/tests/cat_calc_core_tests.cpp
+++ src/tests/cat_calc_core_tests.cpp
@@ -2,49 +2,213 @@
 #include <gmock/gmock.h>
 #include "cat_calc_core.h"
 
+/** Testovaná přesnost kalkulačky */
+#define PRECISION 0.000000000000001
 
 using namespace ::testing;
+using namespace std;
 
-TEST(Addition, Integer){
-    EXPECT_STREQ("29", calculate("5 + 24"));
-    EXPECT_STREQ("29", calculate("5+24"));
-    const char* expectedResult = "9209966124358";
-    EXPECT_STREQ(expectedResult,
-        calculate("8656423001235 + 553543123123"));
-    EXPECT_STREQ("0", calculate("0 + 0"));
+/**
+ * @brief Porovnává 2 vektory řetězců
+ * 
+ * @return True, pokud mají vektory stejný obsah, jinak false
+*/
+bool compareVectors(vector<string> vec1, vector<string> vec2) {
+    if (vec1.size() != vec2.size()) {
+        return false;
+    }
+    
+    for (size_t i = 0; i < vec1.size(); i++) {
+        if (vec1[i] != vec2[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-TEST(Addition, Float){
-    EXPECT_STREQ("29.0131", calculate("5.004 + 24.0091"));
-    EXPECT_STREQ("29.0131", calculate("5.004+24.0091"));
-    const char* result = calculate("785463921.0049687712 + 548736291.0091146878");
-    EXPECT_STREQ("1334200212.014083459", result);
-    EXPECT_STREQ("57.9877", calculate("7.218 + 41.5697 + 2.2 + 7"));
-    EXPECT_STREQ("57.9877", calculate("7.218+41.5697+2.2+7"));
+
+TEST(Calculate, BasicExpressions){
+    string expression = "4";
+    string expectedResult= "4";
+    EXPECT_STREQ(expectedResult.c_str(), calculate(expression).c_str());
+
+    expression = "";
+    expectedResult= "";
+    EXPECT_STREQ(expectedResult.c_str(), calculate(expression).c_str());
+    
+    expression = "     ";
+    expectedResult= "";
+    EXPECT_STREQ(expectedResult.c_str(), calculate(expression).c_str());
+
+    expression = "2!!";
+    expectedResult= "24";
+    EXPECT_STREQ(expectedResult.c_str(), calculate(expression).c_str());
+
+    expression = "4 - (-8)";
+    expectedResult= "12";
+    EXPECT_STREQ(expectedResult.c_str(), calculate(expression).c_str());
+
+    expression = "3.1+ 8.2";
+    expectedResult= "11.3";
+    EXPECT_STREQ(expectedResult.c_str(), calculate(expression).c_str());
 }
 
-TEST(Subtraction, Integer){
-    EXPECT_STREQ("19", calculate("24 - 5"));
-    EXPECT_STREQ("19", calculate("24-5"));
-    EXPECT_STREQ("-19", calculate("5 - 24"));
-    EXPECT_STREQ("-19", calculate("5-24"));
-    const char* expectedResult = "8102879878112";
-    EXPECT_STREQ(expectedResult,
-        calculate("8656423001235 - 553543123123"));
-    EXPECT_STREQ("0", calculate("0 - 0"));
+TEST(Calculate, ComplexExpressions){
+    string expression = "(3.1+ 8.2) * 3 #cos(48)^2";
+    string expectedResult= "8.644748712019314";
+    EXPECT_STREQ(expectedResult.c_str(), calculate(expression).c_str());
+
+    expression = "sin(4!) *10";
+    expectedResult= "4.067366430758002";
+    EXPECT_STREQ(expectedResult.c_str(), calculate(expression).c_str());
+
+    expression = "((sin(4!)+1) *10)+2";
+    expectedResult= "16.067366430758002";
+    EXPECT_STREQ(expectedResult.c_str(), calculate(expression).c_str());
 }
 
-TEST(Subtraction, Float){
-    EXPECT_STREQ("19.0051", calculate("24.0091 - 5.004"));
-    EXPECT_STREQ("19.0051", calculate("24.0091-5.004"));
-    EXPECT_STREQ("-19.0051", calculate("5.004 - 24.0091"));
-    EXPECT_STREQ("-19.0051", calculate("5.004-24.0091"));
-    const char* result = calculate("785463921.0049687712 - 548736291.0091146878");
-    EXPECT_STREQ("236727629.9958540834", result);
-    EXPECT_STREQ("-43.5517", calculate("7.218 - 41.5697 - 2.2 - 7"));
-    EXPECT_STREQ("-43.5517", calculate("7.218-41.5697-2.2-7"));
+TEST(Calculate, Errors){
+    string expression = "3*8/(2-2)";
+    EXPECT_ANY_THROW(calculate(expression));
+
+    expression = "tan(40)!";
+    EXPECT_ANY_THROW(calculate(expression));
+
+    expression = "(-4)!";
+    EXPECT_ANY_THROW(calculate(expression));
+
+    expression = "3*5)+4";
+    EXPECT_ANY_THROW(calculate(expression));
+
+    expression = "2#-4";
+    EXPECT_ANY_THROW(calculate(expression));
+
+    expression = "3#-8";
+    EXPECT_NO_THROW(calculate(expression));
 }
 
+TEST(FormatCheck, Correct){
+    string expression = "3 + (-8)";
+    EXPECT_NO_THROW(formatCheck(expression));
+    expression = "3 - (-8)";
+    EXPECT_NO_THROW(formatCheck(expression));
+    expression = "89++9";
+    EXPECT_NO_THROW(formatCheck(expression));
+    expression = "3";
+    EXPECT_NO_THROW(formatCheck(expression));
+    expression = "-3";
+    EXPECT_NO_THROW(formatCheck(expression));
+    expression = "3!!";
+    EXPECT_NO_THROW(formatCheck(expression));
+
+}
+
+TEST(FormatCheck, Incorrect){
+    string expression = "3*/4";
+    EXPECT_ANY_THROW(formatCheck(expression));
+    expression = "*89+9";
+    EXPECT_ANY_THROW(formatCheck(expression));
+    expression = "89+9*";
+    EXPECT_ANY_THROW(formatCheck(expression));
+    expression = "89+9-";
+    EXPECT_ANY_THROW(formatCheck(expression));
+}
+
+TEST(Parse, BasicExpressions){
+    vector<string> expected = {"5"};
+    vector<string> result = parse("5");
+    EXPECT_PRED2(compareVectors, expected, result);
+
+    expected = {"5.4", "+", "4", "!"};
+    result = parse("5.4    +4 ! ");
+    EXPECT_PRED2(compareVectors, expected, result);
+}
+
+TEST(Parse, ComplexExpressions){
+    vector<string> expected = {"558", "*", "0.5", "-", "(", "-", "2", ")",
+                                "+", "2", "#", "3", "^", "4"};
+    vector<string> result = parse(" 558*0.5 - (-2) + 2#3^4 ");
+    EXPECT_PRED2(compareVectors, expected, result);
+}
+
+TEST(Postfix, BasicExpressions){
+    vector<string> expected = {"5"};
+    vector<string> result = postfix({"5"});
+    EXPECT_PRED2(compareVectors, expected, result);
+
+    expected = {"5.4", "4", "!", "+"};
+    result = postfix({"5.4", "+", "4", "!"});
+    EXPECT_PRED2(compareVectors, expected, result); //This is the line that doesn't work
+}
+
+TEST(Postfix, ComplexExpressions){
+    vector<string> expected = {"558", "0.5", "*", "-2", "-",
+                                "2", "+", "3", "4", "^", "#"};
+    vector<string> result = postfix({"558", "*", "0.5", "-", "(", "-",
+                                    "2", ")", "+", "2", "#", "3", "^", "4"});
+    EXPECT_PRED2(compareVectors, expected, result);
+}
+
+
+TEST(Evaluate, BasicExpressions){
+    string expected = "5";
+    string result = evaluate({"5"});
+    EXPECT_STREQ(expected.c_str(), result.c_str());
+
+    expected = "29.4";
+    result = evaluate({"5.4" "4" "!" "+"});
+    EXPECT_STREQ(expected.c_str(), result.c_str());
+}
+
+TEST(Evaluate, ComplexExpressions){
+    string expected = "290";
+    string result = evaluate({"558", "0.5", "*", "-2", "-",
+                                "2", "+", "3", "4", "^", "#"});
+    EXPECT_STREQ(expected.c_str(), result.c_str());
+}
+
+TEST(Operations, AbsoluteValue){
+    EXPECT_EQ(15, absVal(15));
+    EXPECT_EQ(15, absVal(-15));
+}
+
+TEST(Operations, Root){
+    EXPECT_EQ(5, root(2,25));
+    EXPECT_EQ(2, root(12,4096));
+    EXPECT_EQ(-3, root(3,-27));
+    EXPECT_NEAR(1.286648351223739, root(3, 2.13), PRECISION);
+    EXPECT_NEAR(1.732050807568877, root(2,3), PRECISION);
+    EXPECT_ANY_THROW(root(2,-16));
+}
+
+TEST(Operations, Power){
+    EXPECT_EQ(64, power(2,8));
+    EXPECT_EQ(81, power(4,3));
+    EXPECT_EQ(16, power(2,-4));
+    EXPECT_EQ(-64, power(3,-4));
+
+}
+
+TEST(Operations, Sine){
+    EXPECT_EQ(0.5, csin(30));
+    EXPECT_EQ(-0.5, csin(-30));
+    EXPECT_EQ(csin(5),csin(365));
+    EXPECT_NEAR(0.939692620785908, csin(70), PRECISION);
+}
+
+TEST(Operations, Cosine){
+    EXPECT_EQ(0.5,ccos(60));
+    EXPECT_EQ(0.5,ccos(-60));
+    EXPECT_EQ(ccos(6),ccos(366));
+    EXPECT_NEAR(0.342020143325668, ccos(70), PRECISION);
+}
+
+TEST(Operations, Tan){
+    EXPECT_EQ(1,ctan(45));
+    EXPECT_EQ(ctan(24),ctan(204));
+    EXPECT_NEAR(2.747477419454622, ctan(70), PRECISION);
+    EXPECT_ANY_THROW(ctan(270));
+}
 
 
 /*** Konec souboru cat_calc_core_tests.cpp ***/
