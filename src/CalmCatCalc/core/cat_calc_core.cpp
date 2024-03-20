f5835f7cd581a1fb60221584c29d9dd10b50cdba
David Krejčí <xkrejcd00@stud.fit.vutbr.cz>
2024-03-20 16:07:32 +0100

    Přídány testy a návrhy hlavních funkcí
    Přídány testy pro obecné i hraniční případy.
    Přidány následující funkce, podrobnější popis v cat_calc_core.h:
    calculate - Vypočítá zadaný výraz.
                Hlavní funkce kalkulačky, ostatní funkce se volají z ní.
    formatCheck - Kontroluje syntax zadaného výrazu.
    parse - Převede daný výraz na tokeny.
    postfix - Uspořádá tokeny do postfixového tvaru.
    evaluate - Vypočítá výsledek z postfixového tvaru.
    absVal - Absolutní hodnota.
    root - Odmocnina
    power - Mocnina
    csin - Vlastní sinus
    ccos - Vlastní cosinus
    ctan - Vlastní tangens

--- src/CalmCatCalc/core/cat_calc_core.cpp
+++ src/CalmCatCalc/core/cat_calc_core.cpp
@@ -1,6 +1,52 @@
 #include "cat_calc_core.h"
 
-const char* calculate(const char* expression){
+using namespace std;
+
+
+string calculate(string expression){
     return expression;
 }
+
+double absVal(double num){
+    return num;
+}
+
+double root(int root, double num){
+    return num;
+}
+
+double power(int exp, double num){
+    return num;
+}
+
+double csin(double ang){
+    return ang;
+}
+
+double ccos(double ang){
+    return ang;
+}
+
+double ctan(double ang){
+    return ang;
+}
+
+void formatCheck(string expression){
+    
+}
+
+vector<string> parse(string expression){
+    vector<string> result;
+    result.push_back(expression);
+    return result;
+}
+
+vector<string> postfix(vector<string> parsedExpression){
+    return parsedExpression;
+}
+
+string evaluate(vector<string> postfixExpression){
+    return postfixExpression.front();
+}
+
 /*** Konec souboru cat_calc_core.cpp ***/
