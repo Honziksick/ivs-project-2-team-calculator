15c05ec9c8420ada588f64974be3197acd0080c9
Jan Kalina <xkalinj00@stud.fit.vutbr.cz>
2024-04-09 16:55:31 +0200

    Příprava hlavičkového souboru pro program 'stddev()'
    Vytvořil jsem hlavičkový soubory na základě návrhu programu na
    výpočet vývěrové směrodatné odchylky `stddev()`. Hlavičkový soubor
    obsahuje zdokumentované deklarace funkcí a konstant, které mám v
    plánu v programu použít.

---
+++ src/profiler/stddev.cpp
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ *                                                                             *
+ * Název projektu:   Calm CatCalc                                              *
+ *                                                                             *
+ * Soubor:           stddev.cpp                                                *
+ * Datum:            01.04.2024                                                *
+ * Poslední změna:   01.04.2024                                                *
+ *                                                                             *
+ * Tým:      Calm CatCalc Coders                                               *
+ *                                                                             *
+ * Členové:  Farkašovský Lukáš    <xfarkal00>                                  *
+ *           Hýža Pavel           <xhyzapa00>                                  *
+ *           Kalina Jan           <xkalinj00>                                  *
+ *           Krejčí David         <xkrejcd00>                                  *
+ *        	                                                                   *
+ ******************************************************************************/
+/**
+ * @file stddev.cpp
+ * @author Jan Kalina \<xkalinj00>
+ * @brief __Program na výpočet výběrové směrodatné odchylky.__
+ * @details TO-DO
+ */
+
+#include "../CalmCatCalc/core/cat_calc_core.h"
+#include "stddev.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <random>
+
+using namespace std;
+
---
+++ src/profiler/stddev.h
@@ -0,0 +1,163 @@
+/*******************************************************************************
+ *                                                                             *
+ * Název projektu:   Calm CatCalc                                              *
+ *                                                                             *
+ * Soubor:           stddev.cpp                                                *
+ * Datum:            01.04.2024                                                *
+ * Poslední změna:   01.04.2024                                                *
+ *                                                                             *
+ * Tým:      Calm CatCalc Coders                                               *
+ *                                                                             *
+ * Členové:  Farkašovský Lukáš    <xfarkal00>                                  *
+ *           Hýža Pavel           <xhyzapa00>                                  *
+ *           Kalina Jan           <xkalinj00>                                  *
+ *           Krejčí David         <xkrejcd00>                                  *
+ *        	                                                                   *
+ ******************************************************************************/
+/**
+ * @file stddev.h
+ * @author Jan Kalina \<xkalinj00>
+ * @brief __Deklarace funkcí pro výpočet výběrové směrodatné odchylky.__
+ * @details TO-DO
+ */
+
+#pragma once
+#ifndef STDDEV_H_
+
+/** @cond */
+#define STDDEV_H_
+/** @endcond */
+
+#include "../CalmCatCalc/core/cat_calc_core.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <random>
+
+#define ROOT 2    /**< Kořen odmocniny používaný ve funkci `root()` */
+#define EXP  2    /**< Exponent používaný ve funkci `power()` */
+#define FILE_PATH "auto_gen.txt"    /**< Název generovaného souboru s náhodným daty */
+#define AUTO_GEN_NUM 5     /**< Počet náhodně generovaných čísel */
+#define AUTO_GEN_MIN 0     /**< Nejmenší možná vygenerovaná hodnota */
+#define AUTO_GEN_MAX 20    /**< Nejmenší možná vygenerovaná hodnota */
+#define PRECISION 10       /**< Přesnost desetinné části vygenerovaných čísel */
+
+
+/**
+ * @brief Výčtový typ pro návratové hodnoty funkcí
+ */
+enum returnValues{
+    OK,             /**< OK: Funkce skončila úspěšně */
+    E_INV_DATA,     /**< Error: Načtení nečíselné hodnoty */
+    E_FILE_OPEN     /**< Error: Nepodařilo se otevřít soubor s vstupními daty */
+};
+
+
+/**
+ * @brief Funkce pro generování náhodných čísel do souboru
+ *
+ * @details Tato funkce generuje náhodná čísla v rozsahu definovaném konstantami
+ *          `PRECISION_MIN` a `PRECISION_MAX` a zapisuje je do souboru určeného
+ *          konstantou `FILE_PATH`. Počet generovaných čísel je určen konstantou
+ *          `AUTO_GEN_NUM`. Generovaná čísla jsou vypsána na standardní výstup a
+ *          `zapsána do souboru s pevnou přesností určenou konstantou
+ *          `PRECISION`.
+ */
+void generateNumbers();
+
+
+/**
+ * @brief Funkce pro načítání čísel z datového proudu
+ *
+ * @details Tato funkce načítá hodnoty ze zadaného datového proudu (může to být
+ *          standardní vstup nebo soubor) do doby, dokud jsou k dispozici data.
+ *          Pokud načtená hodnota není číslo, funkce vyhodí výjimku typu
+ *          `invalid_argument`.
+ *
+ * @param dataStream Reference na datový proud (`istream`), ze kterého se
+ *                   načítají data
+ * @param valueSum Reference na řetězec, který uchovává součet načtených hodnot
+ * @param powerSum Reference na řetězec, který uchovává součet druhých mocnin
+ *                 načtených hodnot
+ * @param N Reference na počítadlo načtených hodnot
+ *
+ * @exception invalid_argument Pokud načtená hodnota není číslo
+ */
+
+void readData(istream& dataStream, string &valueSum, string &powerSum, int &N);
+
+/**
+ * @brief Funkce pro načítání čísel ze souboru
+ *
+ * @details Tato funkce otevírá soubor určený konstantou `FILE_PATH` a volá
+ *          funkci `readData()` s otevřeným souborem jako parametrem, což
+ *          umožňuje načítání dat přímo ze souboru. Pokud se soubor nepodaří
+ *          otevřít, vypíše chybovou zprávu "Unable to open file".
+ *
+ * @param valueSum Reference na řetězec, který uchovává součet načtených hodnot
+ * @param powerSum Reference na řetězec, který uchovává součet druhých mocnin
+ *                 načtených hodnot
+ * @param N Reference na počítadlo načtených hodnot
+ *
+ * @see readData()
+ */
+void readDataFromAutoGenFile(string &valueSum, string &powerSum, int &N);
+
+
+/**
+ * @brief Funkce pro načítání čísel ze standardního vstupu (`stdin`)
+ *
+ * @details Tato funkce volá funkci `readData()` s `cin` jako parametrem, což
+ *          umožňuje načítání dat přímo ze standardního vstupu. Je určena
+ *          k použití, když je program spuštěn s přesměrováním vstupu ze
+ *          souboru, například `./stddev < data.txt`.
+ *
+ * @param valueSum Reference na řetězec, který uchovává součet načtených hodnot
+ * @param powerSum Reference na řetězec, který uchovává součet druhých mocnin
+ *                 načtených hodnot
+ * @param N Reference na počítadlo načtených hodnot
+ *
+ * @see readData()
+ */
+void readDataFromStdin(string &valueSum, string &powerSum, int &N);
+
+
+/**
+ * @brief Funkce na výpočet výběrové směrodatné odchylky
+ *
+ * @note V případě, že je funkce spuštěna jako `./stddev < data.txt`, je
+ *       výběrová směrodatná odchylka vypočtena z hodnot v souboru `data.txt`.
+ *       V opačném případě je generátor náhodných čísel vygenerován soubor
+ *       s daty `auto_gen.txt` a vstup programu je braný právě z tohoto souboru.
+ *
+ * @return `standardDeviation` Výběrová směrodatná odchylka
+ */
+double standardDeviation();
+
+
+/**
+ * @brief Hlavní funkce programu
+ *
+ * @details Tato funkce volá funkci `standardDeviation()` pro výpočet výběrové
+ *          směrodatné odchylky. Výsledek je vypsán na standardní výstup
+ *          (`stdout`). Pokud funkce standardDeviation() vyhodí výjimku typu
+ *          `invalid_argument`, chybová zpráva je vypsána na standardní chybový
+ *          výstup (`stderr`) a funkce `main()` vrátí `1`. Pokud nedošlo k žádné
+ *          chybě, funkce `main()` vrátí `0`.
+ *
+ * @return OK Pokud nedošlo k žádné chybě
+ * @return E_INV_DATA Pokud byla vyhozena výjimku typu `invalid_argument`
+ * @return E_FILE_OPEN Pokud byla vyhozena výjimka typu `runtime_error`
+ *
+ * @exception invalid_argument Pokud funkce `standardDeviation()` vyhodí výjimku
+ *            tohoto typu
+ * @exception runtime_error Pokud funkce `standardDeviation()` vyhodí výjimku
+ *            tohoto typu
+ */
+int main();
+
+
+#endif // STDDEV_H_
+
+/*** Konec souboru stddev.h ***/
